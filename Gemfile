a2760b6f9d492a5d799a64d2c5f0a827d1de66d4
Tyler Rick <tyler@k3integrations.com>
2012-06-07 18:01:32 -0700

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,11 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+
+gem 'haml-rails'
+
+group :cucumber do
+  gem 'cucumber'
+  gem 'cucumber-rails'
+  gem 'capybara', git: 'git://github.com/jnicklas/capybara.git'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapybaraNestedSynchronizeBug::Application.config.secret_token = 'eca147362e0a617627de494f0ea325eb3ae5e7b7f3b77e08e53e0bbbaf5d008998c5a77af386633887ef8e1cbb3bc69854d616d17a151f7af0c35dbd45025f9c'
